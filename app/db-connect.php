e69ff05889049a4fcdb0ffab11310e9c1e3850fb
Harshit Shukla <109151752+HarshitShukla-dev@users.noreply.github.com>
2024-11-12 23:37:38 +0530

    Add files via upload

---
+++ app/db-connect.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+// Database connection details
+$host = "educomm-bhism.j.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_sUsweFQEc3-FUEnwql7";
+$db_name = "defaultdb";
+$port = 24334;
+
+// SSL certificate
+$ssl_ca = 'ca.pem';
+
+// Create connection variable with SSL
+$link = mysqli_init();
+if (!$link) {
+    die('mysqli_init failed');
+}
+
+mysqli_ssl_set($link, NULL, NULL, $ssl_ca, NULL, NULL);
+if (!mysqli_real_connect($link, $host, $username, $password, $db_name, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// Check connection
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+?>
